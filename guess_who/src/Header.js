b170feda9915403b86229954bf663f6bba94d79a
becks045 <becks045@umn.edu>
2020-05-28 17:20:54 -0500

    Dropdown implemented

--- guess_who/src/Header.js
+++ guess_who/src/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Grid, Button, Segment } from 'semantic-ui-react';
+import { Header, Grid, Button, Segment, Dropdown } from 'semantic-ui-react';
 
 const API = 'https://guess-who-server12.herokuapp.com';
 
@@ -7,15 +7,16 @@ export default class AppHeader extends Component {
   constructor() {
     super();
     this.state = {
-      submittedName: "",
-      opponentName: ""
+      sets: [],
+      processed: false,
+      selected: false
     }
-
+    this.getAvailableSets();
     this.choose = this.choose.bind(this);
   }
 
-  choose = event => {
-    let choice = event.currentTarget.id;
+  choose = (event, data) => {
+    let choice = data.value;
     const req = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -24,13 +25,38 @@ export default class AppHeader extends Component {
     fetch(API + '/getImages/setChoice', req)
       .then(res => res.json())
         .then(data => console.log(data));
+    this.setState({
+      selected: true
+    })
   }
+
+  getAvailableSets = async () => {
+    await fetch(API + '/getImages/getSets')
+      .then(res => res.text())
+        .then(res => this.setState(state => {
+          const sets = state.sets.concat(JSON.parse(res).sets);
+
+          return {
+            sets,
+            processed: true
+          }
+        }))
+          .catch(err => console.log(err));
+  }
+
 
   handleReset = async e => {
     await fetch(API + '/getImages/reset')
       .then(res => res.text())
         .then(res => console.log(JSON.parse(res).message))
           .catch(err => console.log(err));
+    // let menu = document.getElementById('selector').getElementsByClassName('text')[0];
+    // console.log(menu);
+    // menu.innerHTML = 'Select Character Set';
+    // document.getElementById('selector').reset();
+    this.setState({
+      selected: false
+    })
     }
 
   render() {
@@ -47,19 +73,18 @@ export default class AppHeader extends Component {
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={2}>
-              <Button id='Office' onClick={this.choose}>The Office</Button>
+              <Dropdown
+                id="selector" 
+                placeholder='Select Character Set'
+                fluid
+                search
+                selection
+                options={this.state.sets}
+                onChange={this.choose}
+                />
             </Grid.Column>
             <Grid.Column width={2}>
-              <Button id='WalkingDead' onClick={this.choose}>The Walking Dead</Button>
-            </Grid.Column>
-            <Grid.Column width={2}>
-              <Button id='Brooklyn' onClick={this.choose}>Brooklyn 99</Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button id='HarryPotter' onClick={this.choose}>Harry Potter</Button>
-            </Grid.Column>
-            <Grid.Column width={2}>
-              <Button onClick={this.handleReset}>Start Over</Button>
+              <Button onClick={this.handleReset}>Reset</Button>
             </Grid.Column>
           </Grid.Row>
         </Grid>
