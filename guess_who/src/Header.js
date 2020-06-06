2befedc79714705d807230fe39439fbeffc62043
becks045 <becks045@umn.edu>
2020-06-06 12:45:57 -0500

    Fully fledged upload function

--- guess_who/src/Header.js
+++ guess_who/src/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Grid, Button, Segment, Dropdown, Input } from 'semantic-ui-react';
+import { Header, Grid, Button, Segment, Dropdown, Input, Modal, Label } from 'semantic-ui-react';
 import axios from 'axios';
 
 // const API = 'https://guess-who-server12.herokuapp.com';
@@ -13,7 +13,15 @@ export default class AppHeader extends Component {
       message: '',
       sets: [],
       processed: false,
-      selected: false
+      selected: false,
+      uploadable: false,
+      nameTaken: false,
+      needName: true,
+      needMoreFiles: true,
+      needLessFiles: false,
+      secretKey: '',
+      auth: true,
+      success: ''
     }
     this.getAvailableSets();
     this.choose = this.choose.bind(this);
@@ -54,14 +62,30 @@ export default class AppHeader extends Component {
       .then(res => res.text())
         .then(res => console.log(JSON.parse(res).message))
           .catch(err => console.log(err));
-    // let menu = document.getElementById('selector').getElementsByClassName('text')[0];
-    // console.log(menu);
-    // menu.innerHTML = 'Select Character Set';
-    // document.getElementById('selector').reset();
-    this.setState({
-      selected: false
-    })
+      this.setState({
+        sets: [],
+        selected: false,
+        success: ''
+      }, this.getAvailableSets);
     }
+
+
+
+  validateUpload = () => {
+    let names = this.state.sets.map((set) => {
+      return set.value;
+    })
+
+    this.setState({
+      nameTaken: (names.includes(this.state.message)),
+      needName: (this.state.message === "" ),
+      needMoreFiles: (this.state.images.length < 24),
+      needLessFiles: (this.state.images.length > 24),
+      uploadable: (
+        !(this.state.images.length < 24) && !(this.state.images.length > 24) && !(this.state.message === "" ) && !(names.includes(this.state.message))
+      )
+    })
+  }
 
   addImageSet = (event) => {
     let images = []
@@ -71,32 +95,62 @@ export default class AppHeader extends Component {
     images = images.filter(image => image.name.match(/\.(jpg|jpeg|png|gif)$/))
     this.setState({
       images
-    })
+    }, this.validateUpload);
   }
 
-  setName = (event, data) => {
+  setName = async (event, data) => {
     let message = data.value;
-    this.setState({
+    await this.setState({
       message
+    });
+    this.validateUpload();
+  }
+
+  setKey = async (event, data) => {
+    let secretKey = data.value;
+    console.log(data.value);
+    this.setState({
+      secretKey
     })
   }
 
-  uploadImages = async e => {
+  uploadImages = e => {
+      const data = new FormData();
+      this.state.images.map((image) => {
+        data.append("image", image, image.name);
+      });
+      data.append("setName", this.state.message);
+      axios.post(API + '/getImages/uploadSet', data)
+        .then(res => {
+          let mes = res.data.Resp;
+          this.setState({
+            success: mes
+          })
+        })
+            .catch(err => console.log(err));
+      
+  }
 
-    const data = new FormData();
-
-    // data.append("setName", this.state.message);
-    this.state.images.map((image) => {
-      data.append("image", image, image.name);
-    });
-
-    console.log(data.getAll('image'));
-    data.append("setName", this.state.message);
-
-    axios.post(API + '/getImages/uploadSet', data)
-       .then(res => console.log(JSON.parse(res).Resp))
-          .catch(err => console.log(err));
-    this.getAvailableSets();
+  startUpload = e => {
+    var req = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ secretKey: this.state.secretKey})
+    };
+    fetch(API + '/getImages/checkKey', req)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          auth: data.auth
+        });
+        if(data.auth) {
+          this.uploadImages(e);
+        } else {
+          console.log('Not accepted');
+        }
+      })
+        .catch(err => console.log(err));
   }
 
   render() {
@@ -127,16 +181,60 @@ export default class AppHeader extends Component {
               <Button onClick={this.handleReset}>Reset</Button>
             </Grid.Column>
             <Grid.Column width={8}>
-              <Input
-                onChange={this.setName}
-                />
-              <Input 
-                type='file' 
-                onChange={this.addImageSet} 
-                placeholder='Add Image Set'
-                multiple
-                />
-              <Button onClick={this.uploadImages}>Submit</Button>
+              <Modal 
+                trigger={<Button>Upload New Set</Button>}
+                onClose={this.handleReset}>
+                <Segment attached='top'>
+                  <Input
+                    label='Name'
+                    onChange={this.setName}
+                    />
+                    {this.state.needName && (
+                      <Label basic color='red' pointing='left'>
+                        Please enter a name
+                      </Label>  
+                    )}
+                    {this.state.nameTaken && (
+                      <Label basic color='red' pointing='left'>
+                         Name is Taken
+                     </Label>
+                    )}
+                    </Segment>
+                <Segment attached>
+                  <Input
+                    type='file' 
+                    onChange={this.addImageSet} 
+                    placeholder='Add Image Set'
+                    multiple
+                    />
+                    {this.state.needLessFiles && (
+                      <Label basic color='red' pointing='left'>
+                       Too many files
+                      </Label>
+                    )}
+                    {this.state.needMoreFiles && (
+                      <Label basic color='red' pointing='left'>
+                        Not enough files
+                      </Label>
+                    )}
+                </Segment>
+                <Segment attached>
+                  <Input
+                  label='Secret Key'
+                  onChange={this.setKey}
+                  />
+                </Segment>
+                <Segment attached='bottom'>
+                  {this.state.uploadable ? (
+                      <Button 
+                        onClick={this.startUpload}>Upload</Button>
+                    ) :(
+                    <Button disabled>Upload</Button>
+                    )}
+                    {this.state.success !== '' &&
+                    (<Label color='green'>{this.state.success}</Label>)}
+                </Segment>
+                </Modal>
             </Grid.Column>
           </Grid.Row>
         </Grid>
